6f624835ad9745e32cca85a44b77442cb849d4e6
Yuan Cao <caoyuan0816@gmail.com>
2018-11-12 11:32:33 -0700

    __init__ method in Cube.py.

--- cube/cube.py
+++ cube/cube.py
@@ -9,7 +9,7 @@ Implementation of class Cube, which used to describe a cube in the real world.
 
 class Cube:
     """
-    Class Cube uses a 6*6 matrix to store color of a rubik's cube.
+    Class Cube uses a 6*9 matrix to store color of a rubik's cube.
     Each row described the color of one side of cube, and these sides ordered by:
         Front -> Back -> Right -> Left -> Up -> Down
     Example:
@@ -19,7 +19,7 @@ class Cube:
          [o, o, o, o, o, o, o, o, o], //Left(Orange)
          [w, w, w, w, w, w, w, w, w], //Up(White)
          [y, y, y, y, y, y, y, y, y]] //Down(Yellow)
-        described a unchanged rubik's cube.
+        described a default rubik's cube.
         See: https://en.wikipedia.org/wiki/Rubik%27s_Cube
     """
 
@@ -31,15 +31,26 @@ class Cube:
             layout: optional. Defualt value is None. User can provide a string
                 to describe the default layout of rubik's cube. We will not check
                 the validity of input layout, please make sure u input a valid
-                layout. Example: "g, g, g, g, g, g, g, g, g, b, b, b, b, b, b,
-                b, b, b, r, r, r, r, r, r, r, r, r, o, o, o, o, o, o, o, o, o,
-                w, w, w, w, w, w, y, y, y, y, y, y, y, y, y" is a valid layout.
-                The order of rows must be same as the cube defination above.
+                layout. Example: "g,g,g,g,g,g,g,g,g,b,b,b,b,b,b,b,b,b,r,r,r,r,r,
+                r,r,r,r,o,o,o,o,o,o,o,o,o,w,w,w,w,w,w,y,y,y,y,y,y,y,y,y" is a
+                valid layout. The order of rows must be same as the cube
+                defination above.
         """
         if layout is None:
-            pass
+            self.cube = [['g', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'g'],
+                         ['b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b'],
+                         ['r', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r'],
+                         ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
+                         ['w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w'],
+                         ['y', 'y', 'y', 'y', 'y', 'y', 'y', 'y', 'y']]
         else:
-            pass
+            if len(layout) != 107:
+                raise ValueError('input layout invalid.')
+            else:
+                self.cube = []
+                layout = layout.split(',')
+                for i in range(6):
+                    self.cube.append(layout[i*9:(i+1)*9])
 
     def F(self, reverse=False):
         pass
@@ -58,3 +69,11 @@ class Cube:
 
     def D(self, reverse=False):
         pass
+
+    def isSolved(self):
+        """
+        Check whether or not current cube has been solved.
+        return:
+            boolean value True or False.
+        """
+        return False
