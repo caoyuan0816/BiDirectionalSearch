56e090cc180bec14c60f72016f0069e3220a8b3f
Yuan Cao <caoyuan0816@gmail.com>
2018-11-15 00:22:10 -0700

    Add canvas widgets to game interface.

--- cube/interface.py
+++ cube/interface.py
@@ -17,14 +17,32 @@ class Interface(Frame):
         interface.mainloop()
     """
     def __init__(self, master=None):
+        """
+        Initialize game Interface.
+        """
         Frame.__init__(self, master)
+        # Set title
         self.master.title('ASU CSE571 Rubik\'s Cube')
+        # Set size of window
+        self.master.geometry("810x400+10+150")
+        # Disable resize
+        self.master.resizable(0, 0)
         self.pack()
         self.createWidgets()
 
     def createWidgets(self):
-        pass
+        """
+        Create widgets inside window.
+        We use absolute positioning to arrange widgets.
+        """
+        # Widget canvas, used to draw rubik's cube
+        self.cv = Canvas(self.master)
+        self.cv['bg'] = 'white' # Background color
+        self.cv['height'] = '400' # Height of canvas
+        self.cv['width'] = '540' # Width of canvas
+        self.cv.place(x=0, y=0)
 
 if __name__ == '__main__':
-    interface = Interface()
-    interface.mainloop()
+    root = Tk()
+    interface = Interface(root)
+    root.mainloop()
