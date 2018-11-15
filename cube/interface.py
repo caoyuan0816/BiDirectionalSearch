db9d6998fdf2f83142a4ee0410e7385252d4c345
Yuan Cao <caoyuan0816@gmail.com>
2018-11-14 23:34:36 -0700

    Add interface.py.

---
+++ cube/interface.py
@@ -0,0 +1,13 @@
+"""
+ASU CSE571 Artificial Intelligence Team Project.
+--------------------------------------------------------------------------------
+Interface.py
+--------------------------------------------------------------------------------
+Implementation the game interface of rubik's cube.
+Using GUI library tkinter.
+"""
+
+import tkinter
+
+if __name__ == '__main__':
+    pass
