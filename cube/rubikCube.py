bebb4aacb9d6b4e6feb2ae983f98903dfec14273
Yuan Cao <caoyuan0816@gmail.com>
2018-11-15 02:08:28 -0700

    Add rubikCube.py.

---
+++ cube/rubikCube.py
@@ -0,0 +1,10 @@
+"""
+ASU CSE571 Artificial Intelligence Team Project.
+--------------------------------------------------------------------------------
+rubikCube.py
+--------------------------------------------------------------------------------
+Main entrance for Rubik's Cube project.
+"""
+
+if __name__ == '__main__':
+    pass
