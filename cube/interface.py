a5b1f5e6569940fdb6287ab4399a52cc52536500
Yuan Cao <caoyuan0816@gmail.com>
2018-11-15 00:05:04 -0700

    Interface.__init__().

--- cube/interface.py
+++ cube/interface.py
@@ -4,10 +4,27 @@ ASU CSE571 Artificial Intelligence Team Project.
 Interface.py
 --------------------------------------------------------------------------------
 Implementation the game interface of rubik's cube.
-Using GUI library tkinter.
 """
 
-import tkinter
+from Tkinter import *
+
+class Interface(Frame):
+    """
+    Class Interface used to visualize Rubik's Cube game solving process.
+    It is implemmented by GUI library Tkinter.
+    To start a gameinterface, just do:
+        interface = Interface()
+        interface.mainloop()
+    """
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.master.title('ASU CSE571 Rubik\'s Cube')
+        self.pack()
+        self.createWidgets()
+
+    def createWidgets(self):
+        pass
 
 if __name__ == '__main__':
-    pass
+    interface = Interface()
+    interface.mainloop()
