91867e7c42fd971466b48f8343734ff9718f7034
Yuan Cao <caoyuan0816@gmail.com>
2018-11-17 04:58:40 -0700

    Add rubikCube.py the main entry of project.

--- cube/rubikCube.py
+++ cube/rubikCube.py
@@ -4,7 +4,22 @@ ASU CSE571 Artificial Intelligence Team Project.
 rubikCube.py
 --------------------------------------------------------------------------------
 Main entrance for Rubik's Cube project.
+There are three primary mode:
+    - single test mode: run a single test using given algorithm. In that mode,
+        the GUI will be automatically showed.
+    - multitest mode: run a set of tests using given algorithm. In that mode,
+        we will not show GUI, only print running result for each test layout.
+
+Usage:
+    -m or --mode [single | multi]
+    -l or --layout [random | layoutName]
+    -a or --algorithm [BFS | DFS | AS | BI]
 """
 
+import os
+
+# Set running path
+ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
+
 if __name__ == '__main__':
-    pass
+    print(ROOT_PATH)
