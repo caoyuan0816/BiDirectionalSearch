85d118163a08c7b85d6944e5aae0856851d03125
CSE360-Team5-Lin <zbao7@asu.edu>
2018-11-18 10:06:33 -0700

    add test cases, and add data structures in utils.py

--- cube/utils.py
+++ cube/utils.py
@@ -47,5 +47,53 @@ def generateRandomSingleTest(operationLength, testName):
     with open(TEST_PATH + '/' + testName, 'w') as output:
         output.write(layout)
 
+"""
+    data structures from pacman
+"""
+
+class Stack:
+    "A container with a last-in-first-out (LIFO) queuing policy."
+    def __init__(self):
+        self.list = []
+
+    def push(self,item):
+        "Push 'item' onto the stack"
+        self.list.append(item)
+
+    def pop(self):
+        "Pop the most recently pushed item from the stack"
+        return self.list.pop()
+
+    def isEmpty(self):
+        "Returns true if the stack is empty"
+        return len(self.list) == 0
+
+class Queue:
+    "A container with a first-in-first-out (FIFO) queuing policy."
+    def __init__(self):
+        self.list = []
+
+    def push(self,item):
+        "Enqueue the 'item' into the queue"
+        self.list.insert(0,item)
+
+    def pop(self):
+        """
+          Dequeue the earliest enqueued item still in the queue. This
+          operation removes the item from the queue.
+        """
+        return self.list.pop()
+
+    def isEmpty(self):
+        "Returns true if the queue is empty"
+        return len(self.list) == 0
+
 if __name__ == '__main__':
     generateRandomSingleTest(12, 'rand_12_1')
+
+
+    generateRandomSingleTest(9, 'rand_12_2')
+
+    generateRandomSingleTest(24, 'rand_12_3')
+
+    generateRandomSingleTest(100, 'rand_12_4')
