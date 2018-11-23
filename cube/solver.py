6c63acd94bf77ee5150256809031ae0d626d1288
CSE360-Team5-Lin <zbao7@asu.edu>
2018-11-23 11:50:46 -0700

    basic solutions BFS, DFS, AS

--- cube/solver.py
+++ cube/solver.py
@@ -11,7 +11,8 @@ Including:
     Bi-directional Search solver. (called BI)
 """
 
-from cube import Cube
+from cube import Cube, utils
+
 
 class Solver:
     """
@@ -19,6 +20,7 @@ class Solver:
     """
     def __init__(self, cube):
         self.cube = cube
+        self.ops = ['F', 'B', 'R', 'L', 'U', 'D' ]
 
     def solve(self):
         pass
@@ -32,4 +34,132 @@ class BFS(Solver):
     """
     def solve(self):
         # TODO
-        pass
+        from collections import deque
+
+        visited = []
+        que = deque()
+        que.append(copy.deepcopy(self.cube))
+        solution = []
+
+        while len(que) != 0 :
+            cur = que.popleft()
+            visited.append(copy.deepcopy(self.cube))
+            if cur.isSolved():
+                #invert list to str
+                solution_str = ','.join(solution)
+
+                return solution_str
+            for op in self.ops:
+                op
+                if self.cube not in visited:
+                    solution.append(op)
+                    que.append(copy.deepcopy(self.cube))
+
+                else:
+                    #reverse
+                    op
+                    op
+                    op
+
+        solution_str = ','.join(solution)
+
+        return solution_str
+        #pass
+
+
+class DFS(Solver):
+    """
+        DFS solver.
+        """
+    def solve(self):
+
+        from collections import deque
+        stack = deque()
+
+        stack.append(copy.deepcopy(self.cube))
+        solution = []
+        visited = []
+
+        while len(stack) != 0:
+            cur = stack.pop()
+            visited.append(copy.deepcopy(self.cube))
+
+            if cur.isSolved():
+                solution_str = ','.join(solution)
+
+                return solution_str
+
+
+            for op in self.ops:
+                op
+                if self.cube not in visited:
+                    solution.append(op)
+                    stack.append(copy.deepcopy(self.cube))
+
+
+                else:
+                    #reverse
+                    op
+                    op
+                    op
+
+        return
+        #pass
+
+
+class AS(Solver):
+    """
+    A * solver.
+    """
+    def solve(self):
+        # TODO
+
+        queue = utils.PriorityQueue()
+        visited = []
+        solution = []
+        queue.append(copy.deepcopy(self.cube), heuristic(self.cube))
+
+        while len(queue) != 0:
+
+            cur = queue.pop()
+            visited.append(copy.deepcopy(self.cube))
+
+            if cur.isSolved():
+                solution_str = ','.join(solution)
+
+                return solution_str
+
+
+            for op in self.ops:
+                op
+                if self.cube not in visited:
+                    solution.append(op)
+                    queue.append(copy.deepcopy(self.cube), heuristic(self.cube))
+
+                else:
+                    #reverse
+                    op
+                    op
+                    op
+
+        return
+        #pass
+
+
+
+
+
+
+    """
+    count how many cubes are different with cube[i][1][1]
+    """
+
+    def heuristic(cube):
+        count = 0
+        for i in range(6):
+            color = cube[i][1][1]
+            for j in range(3):
+                for k in range(3):
+                    if cube[i][j][k] != color:
+                        count += 1
+        return count
--- cube/utils.py
+++ cube/utils.py
@@ -8,6 +8,7 @@ Tools for rubik cube project.
 
 import random
 import os
+import heapq
 
 from cube import Cube
 
@@ -56,3 +57,42 @@ if __name__ == '__main__':
     generateRandomSingleTest(24, 'rand_12_3')
 
     generateRandomSingleTest(100, 'rand_12_4')
+
+
+class PriorityQueue:
+    """
+      Implements a priority queue data structure. Each inserted item
+      has a priority associated with it and the client is usually interested
+      in quick retrieval of the lowest-priority item in the queue. This
+      data structure allows O(1) access to the lowest-priority item.
+    """
+    def  __init__(self):
+        self.heap = []
+        self.count = 0
+
+    def push(self, item, priority):
+        entry = (priority, self.count, item)
+        heapq.heappush(self.heap, entry)
+        self.count += 1
+
+    def pop(self):
+        (_, _, item) = heapq.heappop(self.heap)
+        return item
+
+    def isEmpty(self):
+        return len(self.heap) == 0
+
+    def update(self, item, priority):
+        # If item already in priority queue with higher priority, update its priority and rebuild the heap.
+        # If item already in priority queue with equal or lower priority, do nothing.
+        # If item not in priority queue, do the same thing as self.push.
+        for index, (p, c, i) in enumerate(self.heap):
+            if i == item:
+                if p <= priority:
+                    break
+                del self.heap[index]
+                self.heap.append((priority, c, item))
+                heapq.heapify(self.heap)
+                break
+        else:
+            self.push(item, priority)
